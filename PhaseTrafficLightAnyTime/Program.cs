0e569b73be045695767d7f1c31e8e7671368a3e9
Артем <iron-man1998@mail.ru>
2024-10-16 19:52:47 +0300

    git init

---
+++ PhaseTrafficLightAnyTime/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PhaseTrafficLightAnyTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new TrafficLight(30, 40);
+        }
+    }
+
+    public class TrafficLight
+    {
+        /// <summary>
+        /// Конструктор создания светофора.
+        /// </summary>
+        /// <param name="timeGreenLight">Время зеленого сигнала светофора.</param>
+        /// <param name="timeRedLight">Время красного сигнала светофора.</param>
+        public TrafficLight(int timeGreenLight, int timeRedLight)
+        {
+            DateTime dateTime = DateTime.Today;
+            DateTime todayWithTime = dateTime.AddHours(0).AddMinutes(5).AddSeconds(0);
+            TimeGreenLight = timeGreenLight;
+            TimeRedLight = timeRedLight;
+            string y = FindPhase(todayWithTime);
+        }
+        public int TimeGreenLight { get; }
+        public int TimeRedLight { get; }
+
+        public string FindPhase(DateTime time)
+        {
+            DateTime startTime = DateTime.Today;
+            TimeSpan timeSpan = time - startTime; //Переменная для разницы во времени
+            int timeDifference = (int)timeSpan.TotalSeconds;
+            int parts = timeDifference % (TimeGreenLight + TimeRedLight);
+            if (parts <= TimeGreenLight)
+                return $"{parts} сек. ЗЕЛЕНОГО";
+            else 
+            {
+                int x = parts - TimeGreenLight;
+                return $"{x} сек. КРАСНОГО";
+            }
+            
+        }
+
+    }
+}
---
+++ PhaseTrafficLightAnyTime/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("PhaseTrafficLightAnyTime")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PhaseTrafficLightAnyTime")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("9305d65b-911c-41a1-83d8-31e942cabf47")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
